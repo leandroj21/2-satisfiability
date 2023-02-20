a48e13b43b2ee14f5d70325279b473480070b5ce
Leandro Jiménez <leandroj21p@gmail.com>
2023-02-20 15:00:57 -0400

    basic structure

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func isSatisfiable(path string) bool {
+	return false
+}
+
+func main() {
+	pathBase := "./data/2sat"
+	fmt.Println("Satisfiability of the 6 files:")
+	for i := 1; i < 7; i++ {
+		if isSatisfiable(pathBase + strconv.Itoa(i) + ".txt") {
+			fmt.Print(1)
+		} else {
+			fmt.Print(0)
+		}
+	}
 }
